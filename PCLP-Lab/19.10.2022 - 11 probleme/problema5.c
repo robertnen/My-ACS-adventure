7cd8271b1b38da7e65cf2078ea8b1d90e9435e69
Lucky8boy <robert.nenciu@yahoo.com>
2022-10-20 18:06:59 +0300

    Update problema5.c

--- PCLP-Lab/19.10.2022 - 11 probleme/problema5.c
+++ PCLP-Lab/19.10.2022 - 11 probleme/problema5.c
@@ -11,7 +11,7 @@ int main() {
     short isNegative = (a < 0 || b < 0 || c < 0) ? 1 : 0;
 
     if(isNegative) printf("NU\n");
-    else (a  <= b + c) ? printf("DA") : printf("NU");
+    else (a <= b + c) ? printf("DA") : printf("NU");
     
     return 0;
 }
