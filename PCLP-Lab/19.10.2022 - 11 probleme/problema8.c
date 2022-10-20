e6d4c289a3cb8de9d6a98eb6cd72439cf2954152
Lucky8boy <robert.nenciu@yahoo.com>
2022-10-20 17:57:39 +0300

    Update problema8.c

--- PCLP-Lab/19.10.2022 - 11 probleme/problema8.c
+++ PCLP-Lab/19.10.2022 - 11 probleme/problema8.c
@@ -8,14 +8,14 @@ int main() {
 
     scanf("%d %d %d", &a, &b, &c);
 
-    if(a < b && b < c) printf("%d %d %d", a, b, c);
-    if(a < c && c < b) printf("%d %d %d", a, c, b);
+    if(a <= b && b <= c) printf("%d %d %d", a, b, c);
+    if(a <= c && c <= b) printf("%d %d %d", a, c, b);
 
-    if(b < a && a < c) printf("%d %d %d", b, a, c);
-    if(b < c && c < a) printf("%d %d %d", b, c, a);
+    if(b <= a && a <= c) printf("%d %d %d", b, a, c);
+    if(b <= c && c <= a) printf("%d %d %d", b, c, a);
 
-    if(c < b && b < a) printf("%d %d %d", c, b, a);
-    if(c < a && a < b) printf("%d %d %d", c, a, b);
+    if(c <= b && b <= a) printf("%d %d %d", c, b, a);
+    if(c <= a && a <= b) printf("%d %d %d", c, a, b);
 
     return 0;
 }
